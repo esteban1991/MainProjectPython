f1b7574555e42e925c5cf68c89de8eaffaecd875
Gisela Paredes Paredes <gisycu@gmail.com>
2022-11-23 16:11:12 -0500

    Add field companyname in model user

--- API/config/settings.py
+++ API/config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep the clssecret key used in production secret!
 SECRET_KEY = "ut$psxte+5)8q@lyd!8+y*_hbg)vgpwh8aj@lo=4w7d!*217rj"
 
 # SECURITY WARNING: don't run with debug turned on in production!
